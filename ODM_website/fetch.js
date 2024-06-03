9265e0ef28a8a94d7d987f5da9efea9537e537cd
filipaorquidea <117471688+filipaorquidea@users.noreply.github.com>
2024-06-03 17:33:06 +0100

    Site

---
+++ ODM_website/fetch.js
@@ -0,0 +1,16 @@
+async function fetchProjectsJSON() {
+    const response = await fetch('https://api.cosmicjs.com/v3/buckets/odm-production/objects?pretty=true&query=%7B%22type%22:%22obras%22%7D&limit=112&skip=0&read_key=3fygwXY2K9KX0vuwGSRQVoYqMegy89dC5yROT4y1gIQvFq0Qez&depth=1&props=slug,title,metadata,');
+    const projects = await response.json();
+    return projects.objects;
+  }
+
+  fetchProjectsJSON().then(projects => {
+    console.log(projects);
+
+    for (let i = 0; i < projects.length; i++) {
+       object = document.createElement("object");
+       console.log(projects[i].slug);
+       p.innerHTML = "<a href=\"objeto.html?id="+projects[i].id+"\">"+projects[i].slug+"</a>";
+       document.body.appendChild(object);
+    }
+  });
